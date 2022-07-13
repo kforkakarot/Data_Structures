5e0da695297a31033ee0fbcae9ef5ef920629252
Ashwani Singh <55833494+kforkakarot@users.noreply.github.com>
2022-07-13 22:07:57 +0530

    Create dfs.cpp

---
+++ dfs.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef unordered_map<int,vector<int>> graph;
+void dfs(graph g,int n, vector<bool>& vis)
+{   vis[n]=true;
+    cout<<n<<" ";
+    for(int i:g[n])
+        if(vis[i]==false)
+            dfs(g,i,vis);    
+    
+}
+int main()
+{   graph g;
+    int edges;
+    int vertex;
+    cin>>edges;
+    cin>>vertex;
+    int t=edges;
+    while(t--){
+        int node1,node2;
+        cin>>node1>>node2;
+        g[node1].push_back(node2);
+    }
+    
+    vector<bool> vis(vertex,false);
+}
